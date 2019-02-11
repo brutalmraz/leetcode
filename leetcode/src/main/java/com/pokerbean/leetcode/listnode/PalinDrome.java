0b848d5a2ee7429a996318d4358638d583e02a76
PokerBean <brutalmraz@gmail.com>
2019-02-12 00:03:31 +0800

    update PlainDrome

--- leetcode/src/main/java/com/pokerbean/leetcode/listnode/PalinDrome.java
+++ leetcode/src/main/java/com/pokerbean/leetcode/listnode/PalinDrome.java
@@ -1,5 +1,7 @@
 package com.pokerbean.leetcode.listnode;
 
+import java.util.Stack;
+
 /**
  * created by IntelliJ IDEA
  *
@@ -9,22 +11,121 @@ package com.pokerbean.leetcode.listnode;
  * @desc: PalinDrome
  */
 public class PalinDrome {
-    public boolean isPalindrome(ListNode listNode){
-        if (listNode == null) {
-            return false;
-        }
-        if (listNode.next == null){
+
+
+    /**
+     * 利用栈结构比较
+     *
+     * @param head
+     * @return
+     */
+    public static boolean isPalindromeByStack(ListNode head) {
+
+        if (head == null || head.next == null) {
             return true;
         }
-        int len = 1;
-        ListNode nowNode =listNode;
-        while (nowNode.next!=null){
-            nowNode = nowNode.next;
-            len += 1;
+        Stack<ListNode> stack = new Stack<>();
+        ListNode slow = head;
+        ListNode fast = head;
+
+        stack.push(slow);
+        while (fast.next != null && fast.next.next !=  null){
+            slow = slow.next;
+            fast = fast.next.next;
+            stack.push(slow);
         }
 
+        // 如果是偶数
+        if (fast.next != null) {
+            slow = slow.next;
+        }
+        ListNode current = slow;
+        while (slow!=null && slow.next != null){
+            // 当前节点和 栈顶比较
+            if (current.val != stack.pop().val) {
+                return  false;
+            }
+            current = current.next ;
+        }
+        return true;
+    }
 
-        return false;
+
+
+    /**
+     * 利用解耦思想，先找出中点，再截取后半段进行链表反转，最后与原链表进行比较
+     *
+     * @param head
+     * @return
+     */
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode origin = head;
+
+        ListNode middle = findMiddle(head);
+        middle.next = reverseListNodeDie(middle.next);
+
+        ListNode pre = origin;
+        ListNode nex = middle.next;
+        while (nex != null && pre != null && nex.val == pre.val) {
+            pre = pre.next;
+            nex = nex.next;
+        }
+        return nex == null;
+    }
+
+    /**
+     * 快慢指针寻找中间节点
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode findMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode min = head;
+        ListNode max = head.next; // 这里注意要先区分开min max ，不然【1,2】这种情况会走两次while，造成中点变成2
+        while (max != null && max.next != null) {
+            min = min.next;
+            max = max.next.next;
+        }
+        return min;
+    }
+
+    /**
+     * 迭代反转链表
+     *
+     * @param origin
+     * @return
+     */
+    public static ListNode reverseListNodeDie(ListNode origin) {
+        if (origin == null) {
+            return origin;
+        }
+        ListNode preNode = null;
+        ListNode nextNode = null;
+        ListNode nowNode = origin;
+        while (nowNode != null) {
+            nextNode = nowNode.next;
+            nowNode.next = preNode;
+            preNode = nowNode;
+            nowNode = nextNode;
+        }
+        return preNode;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+//        ListNode l3 = new ListNode(1);
+        l1.next = l2;
+//        l2.next = l3;
+        boolean palindrome = isPalindrome(l1);
+        System.out.println(palindrome);
     }
 
 }
--- leetcode/src/test/java/com/pokerbean/leetcode/LeetcodeApplicationTests.java
+++ leetcode/src/test/java/com/pokerbean/leetcode/LeetcodeApplicationTests.java
@@ -1,5 +1,7 @@
 package com.pokerbean.leetcode;
 
+import com.pokerbean.leetcode.listnode.ListNode;
+import com.pokerbean.leetcode.listnode.ReverseListNode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +11,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class LeetcodeApplicationTests {
 
+
+
     @Test
     public void contextLoads() {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        listNode1.next =listNode2;
+        listNode2.next =listNode3;
+        listNode3.next =listNode4;
+        ReverseListNode service = new ReverseListNode();
+//        ListNode reverseListNode = service.reverseListNode(listNode1);
+        ListNode reverseListNode2 = service.reverseListNodeDie(listNode1);
+
+//        System.out.println(reverseListNode);
+        System.out.println(reverseListNode2);
     }
 
 }
